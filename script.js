4c35cacd9224f7e0ed62021a5ba421d0c3fcbdc0
bon nocos <bon.nocos@gmail.com>
2022-04-05 15:23:48 +0800

    added operate function, added number buttons and operators

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
+// add
 function add(a,b) {
     return a + b
 }
 
+// subtract
 function subtract(a,b) {
     return a - b
 }
 
+// multiply
 function multiply(a,b) {
     return a * b
 }
 
+
+// divide
 function divide(a,b) {
     return a / b
+}
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case 'x':
+            return multiply(a, b);
+            break;
+        case '/':
+            if(b === 0) {
+                return null
+            } else {
+                return divide(a, b)
+            }
+            break;
+        default:
+            return null;
+    }
 }
