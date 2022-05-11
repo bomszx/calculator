ad67fa8c39df7041f88f7787570ce3ea7e4802b4
bon nocos <bon.nocos@gmail.com>
2022-05-12 02:24:51 +0800

    function calculator

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ let secondNo;
 let thirdNo
 let a;
 let b;
+let c;
 let operator = "";
 
 
@@ -72,13 +73,7 @@ function operate(operator, a, b) {
 
 // Event listeners
 buttons.forEach(button => {
-    button.addEventListener('click', function(e) {
-        if(!operator) {
-            displayOne(e)
-        }  else {
-            displayTwo(e)
-        }
-    })
+    button.addEventListener('click', populateDisplay)
 })
 
 buttonOperator.forEach(button => {
@@ -88,60 +83,34 @@ buttonOperator.forEach(button => {
 equalsBtn.addEventListener('click', equals) 
 
 
-//convert the values into int, then the logic should allow for a single pair of numbers to be evaluated first!
-
-// we want a function/logic that would run the operate function everytime an operator is clicked
-
-/*
-    - click numpad for numbers
-    - numbers that will show on screen will be our first value if an operator btn is pressed after the numbers
-    - next numbers on screen after an op btn is pressed will be our second value
-    - after the second set of numbers, if an operator button is pressed it should evaluate that pair
-
-    if first no is ''?, we assign the no. on the screen as the first no when we pressed an operator, after pressing an op, we clear the screen in prep for the second no.
-
-    the next numbers on the screen will be evaluated based on the operator that has been pressed if we eigher press '=' or another operator key
-
-    find a way to get values without removing the numbers from the topScreen
-*/
-
 // populateDisplay function
-// function populateDisplay(e) {
-    
-//     if(!operator) {
-//         let num = e.target.value;
-//         topScreen.append(num);
-//         a = parseInt(topScreen.innerText);
-//         console.log(a)
-//     } else if(a) {
-//         let numTwo = e.target.value;
-//         topScreen.append(numTwo);
-//         b = parseInt(topScreen.innerText);
-//         console.log(b)
-//     }
-// }
+function populateDisplay(e) {
+     if(!operator) {
+        let num = e.target.value;
+        topScreen.append(num);
+        a = parseInt(topScreen.innerText);
+        console.log(a)
+    } else if(a) {
+        let numTwo = e.target.value;
+        topScreen.append(numTwo);
+        b = parseInt(topScreen.innerText);
+        console.log(b)
 
-function displayOne(e) {
-    topScreen.append(e.target.value)
-    a = parseInt(topScreen.innerText);
-}
+    } 
 
-function displayTwo(e) {
-    topScreen.append(e.target.value)
-    b = parseInt(topScreen.innerText);
 }
 
 // press an operator button
 function clickOp(e) {
-    operator = e.target.value;
-    topScreen.append(operator)   
-  
+        operator = e.target.value;
+        topScreen.append(operator)   
+        topScreen.innerText = '';
 }
 
 //  press equals button
 function equals(e) {
     console.log(e.target.value)
 
-    thirdNo = operate(operator, a, b)
-    solutionScreen.append(thirdNo)
+    console.log(operate(operator, a,b))
+        solutionScreen.append((operate(operator,a,b)))
 }
