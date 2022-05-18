025d4eca158ae96bb62ee8f1c0b04fe4e1953d33
bon nocos <bon.nocos@gmail.com>
2022-05-18 14:06:53 +0800

    refactor functions/logic

--- script.js
+++ script.js
@@ -1,116 +1,60 @@
-// dom elements
-let topScreen = document.querySelector('#topScreen');
-let solutionScreen = document.querySelector('#solution');
-let buttons = document.querySelectorAll('#button');
-let clearBtn = document.querySelector('#clearButton')
-let buttonOperator = document.querySelectorAll('#button-operator');
-let equalsBtn = document.querySelector('#button-equals')
-let input = document.querySelector('#prevOp')
-
-/*
-    1. input numbers on screen
-    2. save the numbers on the screen after an operator button has been pressed
-    3. operate on the values when 'equals' is pressed
-    4. update screen with 'solution' value
-*/ 
-
-// Variables
-let firstNo;
-let secondNo;
-let thirdNo
+let tops = document.querySelector('#top-screen');
+let middle = document.querySelector('#middle-screen');
 let a;
 let b;
 let c;
-let operator = "";
+let operator;
 
+const buttons = document.querySelectorAll('#button')
+const operatorBtn = document.querySelectorAll('#button-operator')
+const equalsBtn = document.querySelector('#button-equals')
+const clearBtn = document.querySelector('#button-clear')
+const delBtn = document.querySelector('#button-delete')
 
-// Arithmetic Functions
-
-// add
-function add(a,b) {
-    return a + b
-}
-
-// subtract
-function subtract(a,b) {
-    return a - b
-}
-
-// multiply
-function multiply(a,b) {
-    return a * b
-}
-
-// divide
-function divide(a,b) {
-    return a / b
-}
-
-// operate function, used a switch statement to switch operators depending on the need
 function operate(operator, a, b) {
-
     switch(operator) {
         case '+':
             return add(a, b);
-            break;
         case '-':
             return subtract(a, b);
-            break;
         case '*':
             return multiply(a, b);
-            break;
         case '/':
             if(b === 0) {
                 return null
             } else {
                 return divide(a, b)
             }
-            break;
         default:
             return null;
     }
 }
 
-// Event listeners
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
 buttons.forEach(button => {
-    button.addEventListener('click', populateDisplay)
+    button.addEventListener('click', e => {
+        console.log(e.target.value)
+    })
 })
 
-buttonOperator.forEach(button => {
-    button.addEventListener('click', clickOp)
+operatorBtn.forEach(button => {
+    button.addEventListener('click', e => {
+        console.log(e.target.value)
+    })
 })
 
-equalsBtn.addEventListener('click', equals) 
-
-
-// populateDisplay function
-function populateDisplay(e) {
-     if(!operator) {
-        let num = e.target.value;
-        topScreen.append(num);
-        a = parseInt(topScreen.innerText);
-        console.log(a)
-    } else if(a) {
-        let numTwo = e.target.value;
-        topScreen.append(numTwo);
-        b = parseInt(topScreen.innerText);
-        console.log(b)
-
-    } 
-
-}
-
-// press an operator button
-function clickOp(e) {
-        operator = e.target.value;
-        topScreen.append(operator)   
-        topScreen.innerText = '';
-}
-
-//  press equals button
-function equals(e) {
+equalsBtn.addEventListener('click', e => {
     console.log(e.target.value)
-
-    console.log(operate(operator, a,b))
-        solutionScreen.append((operate(operator,a,b)))
-}
+})
